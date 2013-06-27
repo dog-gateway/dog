fe4e0ec43b734981cc14aeb038cfb92e5a45c19e
Claudio Degioanni <claudiodegio@gmail.com>
2013-06-27 19:19:14 +0200

    add measure dao implemtation

---
+++ it.polito.elite.dog.addons.h2eventstore.test/src/it/polito/elite/dog/addons/h2eventstore/test/MeasureDaoImplTest.java
@@ -0,0 +1,14 @@
+package it.polito.elite.dog.addons.h2eventstore.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MeasureDaoImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
