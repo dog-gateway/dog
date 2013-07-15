1e617698c0bb388d4b6701b35d6035e10f8b8fb1
Dario Bonino <dario.bonino@polito.it>
2013-07-15 16:12:09 +0200

    fixed crash on state null...

--- it.polito.elite.dog.admin.system.devicemonitor/src/it/polito/elite/dog/admin/system/devicemonitor/DeviceMonitor.java
+++ it.polito.elite.dog.admin.system.devicemonitor/src/it/polito/elite/dog/admin/system/devicemonitor/DeviceMonitor.java
@@ -99,7 +99,7 @@ public class DeviceMonitor implements DeviceMonitorInterface
 		StringBuffer htmlOut = new StringBuffer();
 		
 		// append the unordered list header
-		//htmlOut.append("<ul class=\"unstyled\">\n");
+		// htmlOut.append("<ul class=\"unstyled\">\n");
 		
 		// get all the installed device services
 		try
@@ -120,12 +120,10 @@ public class DeviceMonitor implements DeviceMonitorInterface
 						
 						// get the device icon...
 						String category = currentDevice.getDeviceDescriptor().getDevCategory();
-						htmlOut.append("<p><i class=\"device-"+category.toLowerCase() + "\"></i>");
+						htmlOut.append("<p><i class=\"device-" + category.toLowerCase() + "\"></i>");
 						
 						// print the device identifier
 						htmlOut.append(currentDevice.getDeviceId() + " ");
-						
-						
 						
 						// get the device activation status
 						String active = (String) allDevices[i].getProperty(DogDeviceCostants.ACTIVE);
@@ -136,7 +134,8 @@ public class DeviceMonitor implements DeviceMonitorInterface
 							if (active.equals("true"))
 							{
 								htmlOut.append("<span class=\"label label-success pull-right\">Active</span></p>");
-								htmlOut.append("<ul style=\"list-style-type: none\"><li>"+this.getDeviceState(currentDevice)+"</li></ul>");
+								htmlOut.append("<ul style=\"list-style-type: none\"><li>"
+										+ this.getDeviceState(currentDevice) + "</li></ul>");
 							}
 							else
 							{
@@ -160,29 +159,35 @@ public class DeviceMonitor implements DeviceMonitorInterface
 		
 		return htmlOut.toString();
 	}
-
+	
 	private String getDeviceState(ControllableDevice currentDevice)
 	{
 		StringBuffer stateAsString = new StringBuffer();
-		DeviceStatus state = ((Controllable)currentDevice).getState();
-		Map<String,State> allStates = state.getStates();
-		
-		//iterate over all states
-		for(String stateKey : allStates.keySet())
+		DeviceStatus state = ((Controllable) currentDevice).getState();
+	
+		//check not null
+		if (state != null)
 		{
-			//get the current state
-			State currentState = allStates.get(stateKey);
+			Map<String, State> allStates = state.getStates();
 			
-			//get the values associate to the current state
-			StateValue currentStateValues[] = currentState.getCurrentStateValue();
-			
-			//iterate over the values
-			stateAsString.append("<p>");
-			for(int i=0; i<currentStateValues.length; i++)
+			// iterate over all states
+			for (String stateKey : allStates.keySet())
 			{
-				stateAsString.append("<span class=\"label label-info\">"+currentStateValues[i].getValue()+"</span>");
+				// get the current state
+				State currentState = allStates.get(stateKey);
+				
+				// get the values associate to the current state
+				StateValue currentStateValues[] = currentState.getCurrentStateValue();
+				
+				// iterate over the values
+				stateAsString.append("<p>");
+				for (int i = 0; i < currentStateValues.length; i++)
+				{
+					stateAsString.append("<span class=\"label label-info\">" + currentStateValues[i].getValue()
+							+ "</span>");
+				}
+				stateAsString.append("<p/>");
 			}
-			stateAsString.append("<p/>");
 		}
 		return stateAsString.toString();
 	}
