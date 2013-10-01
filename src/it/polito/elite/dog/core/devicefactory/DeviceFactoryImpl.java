2405bee0f6204592a3b1e0bc13e157ce8114d361
Luigi <luigi.derussis@polito.it>
2013-10-01 16:58:49 +0200

    working on adding/removing and updating devices in runtime...

--- src/it/polito/elite/dog/core/devicefactory/DeviceFactoryImpl.java
+++ src/it/polito/elite/dog/core/devicefactory/DeviceFactoryImpl.java
@@ -17,13 +17,12 @@
  */
 package it.polito.elite.dog.core.devicefactory;
 
+import it.polito.elite.dog.core.devicefactory.api.DeviceFactory;
 import it.polito.elite.dog.core.housemodel.api.HouseModel;
 import it.polito.elite.dog.core.library.model.AbstractDevice;
 import it.polito.elite.dog.core.library.model.DeviceCostants;
 import it.polito.elite.dog.core.library.model.DeviceDescriptor;
 import it.polito.elite.dog.core.library.model.devicemodel.DogControllable;
-import it.polito.elite.dog.core.library.model.notification.core.AddedNewDeviceNotification;
-import it.polito.elite.dog.core.library.model.notification.core.RemovedDeviceNotification;
 import it.polito.elite.dog.core.library.util.LogHelper;
 
 import java.lang.reflect.Constructor;
@@ -35,23 +34,19 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.device.Device;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventConstants;
-import org.osgi.service.event.EventHandler;
 import org.osgi.service.log.LogService;
 
 /**
  * This class listens for the HouseModel and creates (Dog)Device instances at
  * runtime
  * 
- * @author <a href="mailto:emiliano.castellina@polito.it">Emiliano
- *         Castellina</a> (original implementation)
+ * @author Emiliano Castellina (original implementation)
  * @author <a href="mailto:luigi.derussis@polito.it">Luigi De Russis</a>
  *         (successive modifications)
  * @see <a href="http://elite.polito.it">http://elite.polito.it</a>
  * 
  */
-public class DeviceFactory implements EventHandler
+public class DeviceFactoryImpl implements DeviceFactory
 {
 	// class prefix for loading DogDevices at runtime
 	private final String classPrefix = DogControllable.class.getPackage().getName();
@@ -59,15 +54,15 @@ public class DeviceFactory implements EventHandler
 	private LogHelper logger;
 	// Bundle context
 	private BundleContext context;
-	// Reference to the registration of the EventHandler
-	private ServiceRegistration<?> srEventHandler;
+	// Reference to the registration of the DeviceFactory
+	private ServiceRegistration<?> srDeviceFactory;
 	// reference to the HouseModel
 	private AtomicReference<HouseModel> houseModel = null;
 	
 	/**
 	 * Class constructor.
 	 */
-	public DeviceFactory()
+	public DeviceFactoryImpl()
 	{
 		this.houseModel = new AtomicReference<HouseModel>();
 	}
@@ -105,7 +100,7 @@ public class DeviceFactory implements EventHandler
 		// set everything to null
 		this.logger = null;
 		this.context = null;
-		this.srEventHandler = null;
+		this.srDeviceFactory = null;
 	}
 	
 	/**
@@ -113,9 +108,9 @@ public class DeviceFactory implements EventHandler
 	 */
 	public void unRegister()
 	{
-		if (this.srEventHandler != null)
+		if (this.srDeviceFactory != null)
 		{
-			this.srEventHandler.unregister();
+			this.srDeviceFactory.unregister();
 		}
 	}
 	
@@ -142,154 +137,6 @@ public class DeviceFactory implements EventHandler
 	}
 	
 	/**
-	 * Handle events related to newly added devices and removed devices (at
-	 * runtime)
-	 */
-	@Override
-	public void handleEvent(Event event)
-	{
-		// get the event topic
-		String eventTopic = event.getTopic();
-		
-		// debug
-		this.logger.log(LogService.LOG_DEBUG, "Received event of topic: " + eventTopic);
-		
-		// request to add a new device
-		if (eventTopic.equals(AddedNewDeviceNotification.notificationTopic))
-		{
-			// log
-			this.logger.log(LogService.LOG_INFO, "AddedNewDeviceNotification received");
-			
-			// get device info
-			AddedNewDeviceNotification notification = (AddedNewDeviceNotification) event
-					.getProperty(EventConstants.EVENT);
-			DeviceDescriptor deviceDescr = notification.getNewDeviceDescriptor();
-			String deviceUri = deviceDescr.getDevURI();
-			
-			// log
-			this.logger.log(LogService.LOG_INFO, "Adding " + deviceUri);
-			
-			// check if the device exists...
-			String deviceFilter = String.format("(%s=%s)", DeviceCostants.DEVICEURI, deviceUri);
-			try
-			{
-				ServiceReference<?> references[] = this.context.getServiceReferences(Device.class.getName(),
-						deviceFilter);
-				if (references == null)
-				{
-					// it creates the device from a property set
-					this.createDeviceFromDeviceDescriptor(deviceDescr);
-				}
-				else
-				{
-					// log a warning
-					this.logger.log(LogService.LOG_WARNING, "Device " + deviceUri
-							+ " already exist. Skipping its creation...");
-				}
-			}
-			catch (Exception e)
-			{
-				// error in the device creation
-				this.logger.log(LogService.LOG_ERROR, "Exception while creating " + deviceDescr, e);
-				
-			}
-		}
-		// request to remove an existing device
-		else if (eventTopic.equals(RemovedDeviceNotification.notificationTopic))
-		{
-			this.removeDevice((RemovedDeviceNotification) event.getProperty(EventConstants.EVENT));
-		}
-		
-	}
-	
-	/**
-	 * Remove a device
-	 * 
-	 * @param notification
-	 *            the RemovedDeviceNotification
-	 */
-	private void removeDevice(RemovedDeviceNotification notification)
-	{
-		// get device info
-		DeviceDescriptor deviceDescr = notification.getRemovedDeviceDescriptor();
-		String deviceUri = deviceDescr.getDevURI();
-		
-		// log
-		this.logger.log(LogService.LOG_INFO, "Removing " + deviceUri);
-		
-		// get the device from the framework
-		String deviceFilter = String.format("(%s=%s)", DeviceCostants.DEVICEURI, deviceUri);
-		try
-		{
-			ServiceReference<?> references[] = this.context.getServiceReferences(Device.class.getName(), deviceFilter);
-			if (references != null)
-			{
-				AbstractDevice device = (AbstractDevice) this.context.getService(references[0]);
-				// remove the device!
-				device.removeDevice();
-				
-				// log success
-				this.logger.log(LogService.LOG_INFO, "DeviceFactory removed " + deviceUri);
-				
-				// log a warning if more than a device exists with
-				// the same URI (it should be impossible by construction, in any
-				// way...)
-				if (references.length > 1)
-				{
-					this.logger.log(LogService.LOG_WARNING, "Found more than one device with the URI:" + deviceDescr
-							+ ". Only the first has been removed.");
-				}
-			}
-			else
-			{
-				// no device found
-				this.logger.log(LogService.LOG_WARNING, deviceUri + " not found. Skipping removing procedure...");
-			}
-		}
-		catch (Exception e)
-		{
-			// error in removing the device
-			this.logger.log(LogService.LOG_ERROR, "Exception while destroying " + deviceDescr, e);
-		}
-		
-	}
-	
-	/**
-	 * Create a device class from a device descriptor
-	 * 
-	 * @param devDescriptor
-	 *            the device descriptor needed to create a new device
-	 * @throws ClassNotFoundException
-	 * @throws NoSuchMethodException
-	 * @throws SecurityException
-	 * @throws InvocationTargetException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * @throws IllegalArgumentException
-	 */
-	private void createDeviceFromDeviceDescriptor(DeviceDescriptor devDescriptor) throws ClassNotFoundException,
-			SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException,
-			IllegalAccessException, InvocationTargetException
-	{
-		// get the class corresponding to the desired DogDevice category
-		Class<?> cls = DeviceFactory.class.getClassLoader().loadClass(
-				this.classPrefix + ".Dog" + devDescriptor.getDevCategory());
-		
-		// create the desired DogDevice instance
-		Class<?> partypes[] = new Class[2];
-		partypes[0] = BundleContext.class;
-		partypes[1] = DeviceDescriptor.class;
-		Constructor<?> ct = cls.getConstructor(partypes);
-		Object arglist[] = new Object[2];
-		arglist[0] = this.context;
-		arglist[1] = devDescriptor;
-		ct.newInstance(arglist);
-		
-		// log success
-		this.logger.log(LogService.LOG_INFO, "Created class " + devDescriptor.toString());
-	}
-	
-	/**
 	 * Parse a configuration message and create the DogDevice instances
 	 * 
 	 * @param devicesProp
@@ -312,6 +159,135 @@ public class DeviceFactory implements EventHandler
 			}
 			
 		}
+	}
+	
+	/**
+	 * Create a device class from a device descriptor
+	 * 
+	 * @param devDescriptor
+	 *            the device descriptor needed to create a new device
+	 * @throws ClassNotFoundException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InvocationTargetException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws IllegalArgumentException
+	 */
+	private void createDeviceFromDeviceDescriptor(DeviceDescriptor devDescriptor) throws ClassNotFoundException,
+			SecurityException, NoSuchMethodException, IllegalArgumentException, InstantiationException,
+			IllegalAccessException, InvocationTargetException
+	{
+		// get the class corresponding to the desired DogDevice category
+		Class<?> cls = DeviceFactoryImpl.class.getClassLoader().loadClass(
+				this.classPrefix + ".Dog" + devDescriptor.getDevCategory());
 		
+		// create the desired DogDevice instance
+		Class<?> partypes[] = new Class[2];
+		partypes[0] = BundleContext.class;
+		partypes[1] = DeviceDescriptor.class;
+		Constructor<?> ct = cls.getConstructor(partypes);
+		Object arglist[] = new Object[2];
+		arglist[0] = this.context;
+		arglist[1] = devDescriptor;
+		ct.newInstance(arglist);
+		
+		// log success
+		this.logger.log(LogService.LOG_INFO, "Created class " + devDescriptor.toString());
+	}
+	
+	@Override
+	// TODO review
+	public void addNewDevice(DeviceDescriptor descriptor)
+	{
+		// update the model
+		houseModel.get().addToConfiguration(descriptor);
+		
+		String deviceUri = descriptor.getDevURI();
+		// log
+		this.logger.log(LogService.LOG_INFO, "Adding " + deviceUri);
+		
+		// check if the device exists...
+		String deviceFilter = String.format("(%s=%s)", DeviceCostants.DEVICEURI, deviceUri);
+		try
+		{
+			ServiceReference<?> references[] = this.context.getServiceReferences(Device.class.getName(), deviceFilter);
+			if (references == null)
+			{
+				// it creates the device from a property set
+				this.createDeviceFromDeviceDescriptor(descriptor);
+			}
+			else
+			{
+				// log a warning
+				this.logger.log(LogService.LOG_WARNING, "Device " + deviceUri
+						+ " already exist. Skipping its creation...");
+			}
+		}
+		catch (Exception e)
+		{
+			// error in the device creation
+			this.logger.log(LogService.LOG_ERROR, "Exception while creating " + descriptor, e);
+			
+		}
+		
+	}
+	
+	@Override
+	// TODO review
+	public void removeDevice(String deviceURI)
+	{
+		// update the model
+		this.houseModel.get().removeFromConfiguration(deviceURI);
+		
+		// log
+		this.logger.log(LogService.LOG_INFO, "Removing " + deviceURI);
+		
+		// get the device from the framework
+		String deviceFilter = String.format("(%s=%s)", DeviceCostants.DEVICEURI, deviceURI);
+		try
+		{
+			ServiceReference<?> references[] = this.context.getServiceReferences(Device.class.getName(), deviceFilter);
+			if (references != null)
+			{
+				AbstractDevice device = (AbstractDevice) this.context.getService(references[0]);
+				// remove the device!
+				device.removeDevice();
+				
+				// log success
+				this.logger.log(LogService.LOG_INFO, "DeviceFactory removed " + deviceURI);
+				
+				// log a warning if more than a device exists with
+				// the same URI (it should be impossible by construction, in any
+				// way...)
+				if (references.length > 1)
+				{
+					this.logger.log(LogService.LOG_WARNING, "Found more than one device with the URI:" + deviceURI
+							+ ". Only the first has been removed.");
+				}
+			}
+			else
+			{
+				// no device found
+				this.logger.log(LogService.LOG_WARNING, deviceURI + " not found. Skipping removing procedure...");
+			}
+		}
+		catch (Exception e)
+		{
+			// error in removing the device
+			this.logger.log(LogService.LOG_ERROR, "Exception while destroying " + deviceURI, e);
+		}
+		
+	}
+	
+	// TODO complete
+	@Override
+	public void updateDevice(DeviceDescriptor descriptor)
+	{
+		// update the HouseModel
+		
+		// get the device from the framework
+		
+		// update the device in the framework
 	}
 }
