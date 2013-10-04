c1bd813e1b73d6dd7cd67f9e8861a7251ed9616b
bonino <dario.bonino@gmail.com>
2013-10-04 11:17:22 +0200

    added javascript for device commands

--- it.polito.elite.dog.admin.ui/resources/js/ajax.js
+++ it.polito.elite.dog.admin.ui/resources/js/ajax.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	loadAjaxContent();
+
 });
 
 // ajax loading of content items having a data-load=ajax attribute,
@@ -13,13 +14,34 @@ function loadAjaxContent() {
 		if (refreshTime != null)
 			setInterval(function() {
 				loadContent(element, src);
+				attachCommandButtons();
 			}, parseInt(refreshTime));
-		else
-			loadContent(this, this.getAttribute('data-src'))
+		else {
+			loadContent(this, this.getAttribute('data-src'));
+			attachCommandButtons();
+		}
 	});
 }
 
 // the actualt content loading function
 function loadContent(element, uri) {
 	jQuery(element).load(uri);
+}
+
+function attachCommandButtons() {
+	$('[command=true]').each(function(index) {
+		var url = this.getAttribute('command-dst');
+		this.onclick = function() {
+			postCommand(url)
+		};
+	});
+}
+
+function postCommand(commandUrl) {
+	$.ajax({
+		url : commandUrl,
+		type : "POST",
+		data: null,
+		contentType : "application/json"
+	});
 }
