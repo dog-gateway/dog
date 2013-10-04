7324f7ec1226cba34849e9b1699d5fad782fa0a1
bonino <dario.bonino@gmail.com>
2013-10-04 11:16:59 +0200

    added device command control

--- it.polito.elite.dog.admin.system.devicemonitor/src/it/polito/elite/dog/admin/system/devicemonitor/DeviceMonitor.java
+++ it.polito.elite.dog.admin.system.devicemonitor/src/it/polito/elite/dog/admin/system/devicemonitor/DeviceMonitor.java
@@ -17,6 +17,7 @@
  */
 package it.polito.elite.dog.admin.system.devicemonitor;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 import it.polito.elite.dog.admin.system.devicemonitor.api.DeviceMonitorInterface;
@@ -49,15 +50,15 @@ public class DeviceMonitor implements DeviceMonitorInterface
 {
 	// the service logger
 	private LogHelper logger;
-	
+
 	// devices per row
 	public static final int devicesPerRow = 3;
 	public static final int spanPerRow = 12;
-	
+
 	// the bundle context reference to extract information on the entire Dog
 	// status
 	private BundleContext context;
-	
+
 	/**
 	 * 
 	 */
@@ -65,7 +66,7 @@ public class DeviceMonitor implements DeviceMonitorInterface
 	{
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
 	 * Bundle activation, stores a reference to the context object passed by the
 	 * framework to get access to system data, e.g., installed bundles, etc.
@@ -76,14 +77,14 @@ public class DeviceMonitor implements DeviceMonitorInterface
 	{
 		// store the bundle context
 		this.context = context;
-		
+
 		// init the logger with a null logger
 		this.logger = new LogHelper(this.context);
-		
+
 		// log the activation
 		this.logger.log(LogService.LOG_INFO, "Activated....");
 	}
-	
+
 	/**
 	 * Prepare the bundle to be deactivated...
 	 */
@@ -91,14 +92,14 @@ public class DeviceMonitor implements DeviceMonitorInterface
 	{
 		// null the context
 		this.context = null;
-		
+
 		// log deactivation
 		this.logger.log(LogService.LOG_INFO, "Deactivated...");
-		
+
 		// null the logger
 		this.logger = null;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -114,15 +115,16 @@ public class DeviceMonitor implements DeviceMonitorInterface
 	{
 		// the output buffer
 		StringBuffer htmlOut = new StringBuffer();
-		
+
 		// append the unordered list header
 		// htmlOut.append("<ul class=\"unstyled\">\n");
-		
+
 		// get all the installed device services
 		try
 		{
-			ServiceReference<?>[] allDevices = this.context.getAllServiceReferences(Device.class.getName(), null);
-			
+			ServiceReference<?>[] allDevices = this.context
+					.getAllServiceReferences(Device.class.getName(), null);
+
 			if (allDevices != null)
 			{
 				for (int i = 0; i < allDevices.length; i++)
@@ -131,7 +133,7 @@ public class DeviceMonitor implements DeviceMonitorInterface
 					if (device instanceof ControllableDevice)
 					{
 						ControllableDevice currentDevice = (ControllableDevice) device;
-						
+
 						// start the list item for the current device
 						if ((i % DeviceMonitor.devicesPerRow) == 0)
 						{
@@ -141,22 +143,26 @@ public class DeviceMonitor implements DeviceMonitorInterface
 								htmlOut.append("</div>");
 							htmlOut.append("<div class=\"row-fluid\">");
 						}
-						
+
 						// format as a grid
-						htmlOut.append("<div class=\"span" + (DeviceMonitor.spanPerRow / DeviceMonitor.devicesPerRow)
+						htmlOut.append("<div class=\"span"
+								+ (DeviceMonitor.spanPerRow / DeviceMonitor.devicesPerRow)
 								+ "\">");
 						htmlOut.append("<div class=\"well\"");
-						
+
 						// get the device icon...
-						String category = currentDevice.getDeviceDescriptor().getDevCategory();
-						htmlOut.append("<p><i class=\"device-" + category.toLowerCase() + "\"></i>");
-						
+						String category = currentDevice.getDeviceDescriptor()
+								.getDevCategory();
+						htmlOut.append("<p><i class=\"device-"
+								+ category.toLowerCase() + "\"></i>");
+
 						// print the device identifier
 						htmlOut.append(currentDevice.getDeviceId() + " ");
-						
+
 						// get the device activation status
-						String active = (String) allDevices[i].getProperty(DeviceCostants.ACTIVE);
-						
+						String active = (String) allDevices[i]
+								.getProperty(DeviceCostants.ACTIVE);
+
 						// render the activation status
 						if ((active != null) && (!active.isEmpty()))
 						{
@@ -164,35 +170,36 @@ public class DeviceMonitor implements DeviceMonitorInterface
 							{
 								htmlOut.append("<span class=\"label label-success pull-right\">Active</span></p>");
 								htmlOut.append("<ul style=\"list-style-type: none\"><li>"
-										+ this.getDeviceState(currentDevice) + "</li></ul>");
-							}
-							else
+										+ this.getDeviceState(currentDevice)
+										+ "</li><li>"
+										+ this.getCommands(currentDevice)
+										+ "</li></ul>");
+							} else
 							{
 								htmlOut.append("<span class=\"label label-warning pull-right\">Not Active</span>");
 							}
 						}
-						
+
 						// close the device-related list item
 						htmlOut.append("</div></div>");
 					}
 				}
-				
+
 				// close orphan divs
 				if ((allDevices.length % DeviceMonitor.devicesPerRow) > 0)
 					htmlOut.append("</div>");
 			}
-		}
-		catch (InvalidSyntaxException e)
+		} catch (InvalidSyntaxException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		htmlOut.append("</ul>\n");
-		
+
 		return htmlOut.toString();
 	}
-	
+
 	@Override
 	@GET
 	@Path("/devices/statistics")
@@ -201,14 +208,15 @@ public class DeviceMonitor implements DeviceMonitorInterface
 	{
 		// the output buffer for storing incrementally built html
 		StringBuffer htmlOut = new StringBuffer();
-		
+
 		// device counts
 		int nDevices = 0;
 		int nActiveDevices = 0;
 		try
 		{
-			ServiceReference<?>[] allDevices = this.context.getAllServiceReferences(Device.class.getName(), null);
-			
+			ServiceReference<?>[] allDevices = this.context
+					.getAllServiceReferences(Device.class.getName(), null);
+
 			if (allDevices != null)
 			{
 				for (int i = 0; i < allDevices.length; i++)
@@ -217,11 +225,12 @@ public class DeviceMonitor implements DeviceMonitorInterface
 					if (device instanceof ControllableDevice)
 					{
 						// get the device activation status
-						String active = (String) allDevices[i].getProperty(DeviceCostants.ACTIVE);
-						
+						String active = (String) allDevices[i]
+								.getProperty(DeviceCostants.ACTIVE);
+
 						// parse the boolean value hold by the active string
 						boolean isDeviceActive = Boolean.valueOf(active);
-						
+
 						// updated device counts
 						nDevices++;
 						if (isDeviceActive)
@@ -229,16 +238,15 @@ public class DeviceMonitor implements DeviceMonitorInterface
 					}
 				}
 			}
-		}
-		catch (InvalidSyntaxException e)
+		} catch (InvalidSyntaxException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// the ratio
 		double activeRatio = (double) nActiveDevices / (double) nDevices;
-		
+
 		// compute the label color
 		String color = "";
 		if (activeRatio < 0.33)
@@ -247,42 +255,77 @@ public class DeviceMonitor implements DeviceMonitorInterface
 			color = "label-success";
 		else
 			color = "label-warning";
-		
-		htmlOut.append("<span class=\"label pull-right " + color + "\">" + nActiveDevices + " Active / " + (nDevices-nActiveDevices) + " Not Active</span>");
-		
+
+		htmlOut.append("<span class=\"label pull-right " + color + "\">"
+				+ nActiveDevices + " Active / " + (nDevices - nActiveDevices)
+				+ " Not Active</span>");
+
 		// return the statistics
 		return htmlOut.toString();
 	}
-	
+
 	private String getDeviceState(ControllableDevice currentDevice)
 	{
 		StringBuffer stateAsString = new StringBuffer();
 		DeviceStatus state = ((Controllable) currentDevice).getState();
-		
+
 		// check not null
 		if (state != null)
 		{
 			Map<String, State> allStates = state.getStates();
-			
+
 			// iterate over all states
 			for (String stateKey : allStates.keySet())
 			{
 				// get the current state
 				State currentState = allStates.get(stateKey);
-				
+
 				// get the values associate to the current state
-				StateValue currentStateValues[] = currentState.getCurrentStateValue();
-				
+				StateValue currentStateValues[] = currentState
+						.getCurrentStateValue();
+
 				// iterate over the values
-				stateAsString.append("<p>"+currentState.getStateName()+" ");
+				stateAsString.append("<p>" + currentState.getStateName() + " ");
 				for (int i = 0; i < currentStateValues.length; i++)
 				{
-					stateAsString.append("<span class=\"label label-info pull-right\">" + currentStateValues[i].getValue()
-							+ "</span>");
+					stateAsString
+							.append("<span class=\"label label-info pull-right\">"
+									+ currentStateValues[i].getValue()
+									+ "</span>");
 				}
 				stateAsString.append("<p/>");
 			}
 		}
 		return stateAsString.toString();
+	}
+
+	private String getCommands(ControllableDevice device)
+	{
+		// prepare the string buffer for holding the HTML string representation
+		// of the device commands
+		StringBuffer commandsAsHTMLString = new StringBuffer();
+
+		Method availableCommands[] = device.getClass().getDeclaredMethods();
+
+		for (int i = 0; i < availableCommands.length; i++)
+		{
+			String commandName = availableCommands[i].getName();
+
+			if ((!commandName.contains("get"))&&(!commandName.contains("notify")))
+			{
+				if (availableCommands[i].getParameterTypes().length == 0)
+				{
+					// render the command
+					commandsAsHTMLString
+							.append("<input type=\"button\" class=\"btn\" command=\"true\" command-dst=\"/services/devices/"
+									+ device.getDeviceId()
+									+ "/commands/"
+									+ commandName +"\" value=\""+commandName+"\"/>");
+				}
+			}
+		}
+
+		// returns the string representation
+		return commandsAsHTMLString.toString();
 	}
 }
