5fb36979fdd5031efa8246a87b9b316731e98e92
Dario Bonino <dario.bonino@polito.it>
2013-10-07 16:52:59 +0200

    Initial skeleton for run-time update of home configuration: device descriptor factory

---
+++ src/it/polito/elite/dog/core/library/model/DeviceDescriptorFactory.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.polito.elite.dog.core.library.model;
+
+/**
+ * 1) given a valid descriptor xml (jaxb Device object) generates a descriptor
+ * instance (by simply calling the constructor)
+ * 
+ * 2) given an hashmap of device-specific parameters and a template
+ * "folder or classpath" generates a descriptor instance by filling the
+ * template, parsing it into a jaxb Device and finally calling the constructor.
+ * 
+ * @author bonino
+ * 
+ */
+public class DeviceDescriptorFactory
+{
+
+}
