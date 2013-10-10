bd93eaee12509edac8baf710e76a6cd39b2e4af4
Luigi <luigi.derussis@polito.it>
2013-10-10 15:30:51 +0200

    add XmlRootElement annotation

--- src/it/polito/elite/dog/core/library/jaxb/DogHomeConfiguration.java
+++ src/it/polito/elite/dog/core/library/jaxb/DogHomeConfiguration.java
@@ -27,8 +27,10 @@ package it.polito.elite.dog.core.library.jaxb;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -52,6 +54,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "dogHomeConfiguration", propOrder = {
     "buildingEnvironment",
