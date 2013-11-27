2fc663eea3f301266cbe8a1391d5becd0410a5dc
Luigi <luigi.derussis@polito.it>
2013-11-27 10:28:28 +0100

    improved compliance with the OSGi specifications

--- it.polito.elite.dog.drivers.zwave.doorwindowsensor/src/it/polito/elite/dog/drivers/zwave/doorwindowsensor/ZWaveDoorWindowSensorDriver.java
+++ it.polito.elite.dog.drivers.zwave.doorwindowsensor/src/it/polito/elite/dog/drivers/zwave/doorwindowsensor/ZWaveDoorWindowSensorDriver.java
@@ -23,6 +23,7 @@ import it.polito.elite.dog.drivers.zwave.network.interfaces.ZWaveNetwork;
 import it.polito.elite.dog.core.library.model.DeviceCostants;
 import it.polito.elite.dog.core.library.model.ControllableDevice;
 import it.polito.elite.dog.core.library.util.LogHelper;
+import it.polito.elite.dog.core.library.model.devicecategory.Controllable;
 import it.polito.elite.dog.core.library.model.devicecategory.WindowSensor;
 
 import java.util.Dictionary;
@@ -45,49 +46,48 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 {
 	// The OSGi framework context
 	protected BundleContext context;
-
+	
 	// System logger
 	LogHelper logger;
-
+	
 	// the log identifier, unique for the class
 	public static String LOG_ID = "[ZWaveDoorWindowSensorDriver]: ";
-
+	
 	// String identifier for driver id
 	public static final String DRIVER_ID = "ZWave_ZWaveDoorWindowSensor_driver";
-
+	
 	// a reference to the network driver
 	private AtomicReference<ZWaveNetwork> network;
-
+	
 	// a reference to the gateway driver
 	private AtomicReference<ZWaveGatewayDriver> gateway;
-
+	
 	// milliseconds between two update of the device status, from configuration
 	// file
 	protected int updateTimeMillis;
-
+	
 	// the list of driver instances currently connected to a device
 	private Vector<ZWaveDoorWindowSensorDriverInstance> connectedDrivers;
-
+	
 	// the registration object needed to handle the life span of this bundle in
 	// the OSGi framework (it is a ServiceRegistration object for use by the
 	// bundle registering the service to update the service's properties or to
 	// unregister the service).
 	private ServiceRegistration<?> regDriver;
-
+	
 	// the filter query for listening to framework events relative to the
 	// to the ZWave gateway driver
-	String filterQuery = String.format("(%s=%s)", Constants.OBJECTCLASS,
-			ZWaveGatewayDriver.class.getName());
-
+	String filterQuery = String.format("(%s=%s)", Constants.OBJECTCLASS, ZWaveGatewayDriver.class.getName());
+	
 	// what are the on/off device categories that can match with this driver?
 	private Set<String> doorWindowSensorCategories;
-
+	
 	public ZWaveDoorWindowSensorDriver()
 	{
 		this.gateway = new AtomicReference<ZWaveGatewayDriver>();
 		this.network = new AtomicReference<ZWaveNetwork>();
 	}
-
+	
 	/**
 	 * Handle the bundle activation
 	 */
@@ -95,118 +95,124 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 	{
 		// init the logger
 		logger = new LogHelper(bundleContext);
-
+		
 		// store the context
 		context = bundleContext;
-
+		
 		// initialize the connected drivers list
 		connectedDrivers = new Vector<ZWaveDoorWindowSensorDriverInstance>();
-
+		
 		// initialize the set of implemented device categories
 		doorWindowSensorCategories = new HashSet<String>();
-
+		
 		// fill supported categories of device
 		properFillDeviceCategories();
 	}
-
+	
 	public void deactivate()
 	{
 		// remove the service from the OSGi framework
 		this.unRegister();
 	}
-
+	
 	public void addingService(ZWaveGatewayDriver gatewayDriver)
 	{
 		gateway.set(gatewayDriver);
-
+		
 		// TODO: remove!!!
 		network.set(gateway.get().getNetwork());
-
+		
 		// this.registerDriver();
-
+		
 	}
-
+	
 	public void removedService(ZWaveGatewayDriver gatewayDriver)
 	{
 		if (gateway.compareAndSet(gatewayDriver, null))
 			// unregisters this driver from the OSGi framework
 			unRegister();
 	}
-
+	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public synchronized int match(ServiceReference reference) throws Exception
 	{
 		int matchValue = Device.MATCH_NONE;
-
-		// get the given device category
-		String deviceCategory = (String) reference
-				.getProperty(DeviceCostants.DEVICE_CATEGORY);
-
-		// get the given device manufacturer
-		String manifacturer = (String) reference
-				.getProperty(DeviceCostants.MANUFACTURER);
-
-		// get the gateway to which the device is connected
-		@SuppressWarnings("unchecked")
-		String gateway = (String) ((ControllableDevice) context
-				.getService(reference)).getDeviceDescriptor().getGateway();
-
-		// compute the matching score between the given device and this driver
-		if (deviceCategory != null)
+		
+		if ((this.network != null) && (this.gateway != null) && (this.regDriver != null))
 		{
-			if (manifacturer != null && (gateway != null)
-					&& (manifacturer.equals(ZWaveInfo.MANUFACTURER))
-					&& (doorWindowSensorCategories.contains(deviceCategory))
-					&& (this.gateway.get().isGatewayAvailable(gateway)))
+			// get the given device category
+			String deviceCategory = (String) reference.getProperty(DeviceCostants.DEVICE_CATEGORY);
+			
+			try
 			{
-				matchValue = WindowSensor.MATCH_MANUFACTURER
-						+ WindowSensor.MATCH_TYPE;
+				// get the device class
+				if (Controllable.class.isAssignableFrom(ZWaveDoorWindowSensorDriver.class.getClassLoader().loadClass(
+						deviceCategory)))
+				{
+					// get the given device manufacturer
+					String manifacturer = (String) reference.getProperty(DeviceCostants.MANUFACTURER);
+					
+					// get the gateway to which the device is connected
+					String gateway = (String) reference.getProperty(DeviceCostants.GATEWAY);
+					
+					// compute the matching score between the given device and
+					// this driver
+					if (deviceCategory != null)
+					{
+						if (manifacturer != null && (gateway != null) && (manifacturer.equals(ZWaveInfo.MANUFACTURER))
+								&& (doorWindowSensorCategories.contains(deviceCategory))
+								&& (this.gateway.get().isGatewayAvailable(gateway)))
+						{
+							matchValue = WindowSensor.MATCH_MANUFACTURER + WindowSensor.MATCH_TYPE;
+						}
+						
+					}
+					
+				}
 			}
-
+			catch (ClassNotFoundException e)
+			{
+				// skip --> no match
+			}
 		}
+		
 		return matchValue;
 	}
-
+	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
-	public synchronized String attach(ServiceReference reference)
-			throws Exception
+	public synchronized String attach(ServiceReference reference) throws Exception
 	{
 		// get the referenced device
-		ControllableDevice device = ((ControllableDevice) context
-				.getService(reference));
+		ControllableDevice device = ((ControllableDevice) context.getService(reference));
 		
 		// get the gateway to which the device is connected
 		String gateway = (String) device.getDeviceDescriptor().getGateway();
-
+		
 		// get the corresponding end point set
-		Set<String> nodeIdSet = device.getDeviceDescriptor()
-				.getSimpleConfigurationParams().get(ZWaveInfo.NODE_ID);
-
+		Set<String> nodeIdSet = device.getDeviceDescriptor().getSimpleConfigurationParams().get(ZWaveInfo.NODE_ID);
+		
 		// get the corresponding end point set
-		Set<String> instanceIdSet = device.getDeviceDescriptor()
-				.getSimpleConfigurationParams().get(ZWaveInfo.INSTANCE_ID);
-
+		Set<String> instanceIdSet = device.getDeviceDescriptor().getSimpleConfigurationParams()
+				.get(ZWaveInfo.INSTANCE_ID);
+		
 		// get the nodeId
 		String sNodeID = nodeIdSet.iterator().next();
-
+		
 		// get the list of instances available
 		HashSet<Integer> instancesId = new HashSet<Integer>();
 		for (String sInstanceId : instanceIdSet)
 			instancesId.add(Integer.parseInt(sInstanceId));
-
+		
 		// create a new driver instance
-		ZWaveDoorWindowSensorDriverInstance driverInstance = new ZWaveDoorWindowSensorDriverInstance(
-				network.get(),
-				device,
-				Integer.parseInt(sNodeID), instancesId, this.gateway.get()
-						.getSpecificGateway(gateway).getNodeInfo()
-						.getDeviceNodeId(), updateTimeMillis, context);
-
+		ZWaveDoorWindowSensorDriverInstance driverInstance = new ZWaveDoorWindowSensorDriverInstance(network.get(),
+				device, Integer.parseInt(sNodeID), instancesId, this.gateway.get().getSpecificGateway(gateway)
+						.getNodeInfo().getDeviceNodeId(), updateTimeMillis, context);
+		
 		// connect this driver instance with the device
 		device.setDriver(driverInstance);
-
+		
 		// store a reference to the connected driver
 		synchronized (connectedDrivers)
 		{
@@ -214,7 +220,7 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 		}
 		return null;
 	}
-
+	
 	/**
 	 * Registers this driver in the OSGi framework, making its services
 	 * available to all the other bundles living in the same or in connected
@@ -222,19 +228,17 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 	 */
 	private void registerDriver()
 	{
-		if ((gateway.get() != null) && (network.get() != null)
-				&& (this.context != null) && (this.regDriver == null))
+		if ((gateway.get() != null) && (network.get() != null) && (this.context != null) && (this.regDriver == null))
 		{
 			// create a new property object describing this driver
 			Hashtable<String, Object> propDriver = new Hashtable<String, Object>();
 			// add the id of this driver to the properties
 			propDriver.put(DeviceCostants.DRIVER_ID, DRIVER_ID);
 			// register this driver in the OSGi framework
-			regDriver = context.registerService(Driver.class.getName(), this,
-					propDriver);
+			regDriver = context.registerService(Driver.class.getName(), this, propDriver);
 		}
 	}
-
+	
 	/**
 	 * Handle the bundle de-activation
 	 */
@@ -247,7 +251,7 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 			regDriver = null;
 		}
 	}
-
+	
 	/**
 	 * Fill a set with all the device categories whose devices can match with
 	 * this driver. Automatically retrieve the device categories list by reading
@@ -255,26 +259,23 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 	 */
 	private void properFillDeviceCategories()
 	{
-		for (Class<?> devCat : ZWaveDoorWindowSensorDriverInstance.class
-				.getInterfaces())
+		for (Class<?> devCat : ZWaveDoorWindowSensorDriverInstance.class.getInterfaces())
 		{
 			doorWindowSensorCategories.add(devCat.getName());
 		}
 	}
-
+	
 	@Override
-	public void updated(Dictionary<String, ?> properties)
-			throws ConfigurationException
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException
 	{
 		if (properties != null)
 		{
 			// try to get the baseline polling time
-			String updateTimeAsString = (String) properties
-					.get(ZWaveInfo.PROPERTY_UPDATETIME);
-
+			String updateTimeAsString = (String) properties.get(ZWaveInfo.PROPERTY_UPDATETIME);
+			
 			// trim leading and trailing spaces
 			updateTimeAsString = updateTimeAsString.trim();
-
+			
 			// check not null
 			if (updateTimeAsString != null)
 			{
@@ -283,12 +284,10 @@ public class ZWaveDoorWindowSensorDriver implements Driver, ManagedService
 			}
 			else
 			{
-				throw new ConfigurationException(ZWaveInfo.PROPERTY_UPDATETIME,
-						ZWaveInfo.PROPERTY_UPDATETIME
-								+ " not defined in configuraton file for "
-								+ ZWaveDoorWindowSensorDriver.class.getName());
+				throw new ConfigurationException(ZWaveInfo.PROPERTY_UPDATETIME, ZWaveInfo.PROPERTY_UPDATETIME
+						+ " not defined in configuraton file for " + ZWaveDoorWindowSensorDriver.class.getName());
 			}
-
+			
 			// register driver
 			registerDriver();
 		}
--- it.polito.elite.dog.drivers.zwave.thermostaticradiatorvalve/src/it/polito/elite/dog/drivers/zwave/thermostaticradiatorvalve/ZWaveThermostaticRadiatorValveDriver.java
+++ it.polito.elite.dog.drivers.zwave.thermostaticradiatorvalve/src/it/polito/elite/dog/drivers/zwave/thermostaticradiatorvalve/ZWaveThermostaticRadiatorValveDriver.java
@@ -19,6 +19,7 @@ package it.polito.elite.dog.drivers.zwave.thermostaticradiatorvalve;
 
 import it.polito.elite.dog.core.library.model.ControllableDevice;
 import it.polito.elite.dog.core.library.model.DeviceCostants;
+import it.polito.elite.dog.core.library.model.devicecategory.Controllable;
 import it.polito.elite.dog.core.library.model.devicecategory.ThermostaticRadiatorValve;
 import it.polito.elite.dog.core.library.model.notification.core.ClockTimeNotification;
 import it.polito.elite.dog.core.library.util.LogHelper;
@@ -51,57 +52,55 @@ import org.osgi.service.event.EventHandler;
  * @author bonino
  * 
  */
-public class ZWaveThermostaticRadiatorValveDriver implements Driver,
-		ManagedService, EventHandler
+public class ZWaveThermostaticRadiatorValveDriver implements Driver, ManagedService, EventHandler
 {
 	// The OSGi framework context
 	protected BundleContext context;
-
+	
 	// System logger
 	LogHelper logger;
-
+	
 	// the log identifier, unique for the class
 	public static String LOG_ID = "[ZWaveTemperatureAndHumiditySensorDriver]: ";
-
+	
 	// String identifier for driver id
 	public static final String DRIVER_ID = "ZWave_ZWaveTemperatureAndHumiditySensorDriver_driver";
-
+	
 	// a reference to the network driver
 	private AtomicReference<ZWaveNetwork> network;
-
+	
 	// a reference to the gateway driver
 	private AtomicReference<ZWaveGatewayDriver> gateway;
-
+	
 	// milliseconds between two update of the device status, from configuration
 	// file
 	protected int updateTimeMillis;
-
+	
 	// the persistence store associated to this driver
 	private String persistenceStoreDirectory;
-
+	
 	// the list of driver instances currently connected to a device
 	private Vector<ZWaveThermostaticRadiatorValveInstance> connectedDrivers;
-
+	
 	// the registration object needed to handle the life span of this bundle in
 	// the OSGi framework (it is a ServiceRegistration object for use by the
 	// bundle registering the service to update the service's properties or to
 	// unregister the service).
 	private ServiceRegistration<?> regDriver;
-
+	
 	// the filter query for listening to framework events relative to the
 	// to the ZWave gateway driver
-	String filterQuery = String.format("(%s=%s)", Constants.OBJECTCLASS,
-			ZWaveGatewayDriver.class.getName());
-
+	String filterQuery = String.format("(%s=%s)", Constants.OBJECTCLASS, ZWaveGatewayDriver.class.getName());
+	
 	// what are the on/off device categories that can match with this driver?
 	private Set<String> thermostaticRadiatorValveDeviceCategories;
-
+	
 	public ZWaveThermostaticRadiatorValveDriver()
 	{
 		this.gateway = new AtomicReference<ZWaveGatewayDriver>();
 		this.network = new AtomicReference<ZWaveNetwork>();
 	}
-
+	
 	/**
 	 * Handle the bundle activation
 	 */
@@ -109,128 +108,135 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 	{
 		// init the logger
 		logger = new LogHelper(bundleContext);
-
+		
 		// store the context
 		context = bundleContext;
-
+		
 		// initialize the connected drivers list
 		connectedDrivers = new Vector<ZWaveThermostaticRadiatorValveInstance>();
-
+		
 		// initialize the set of implemented device categories
 		thermostaticRadiatorValveDeviceCategories = new HashSet<String>();
-
+		
 		// fill supported categories of device
 		properFillDeviceCategories();
 	}
-
+	
 	public void deactivate()
 	{
 		// remove the service from the OSGi framework
 		this.unRegister();
 	}
-
+	
 	public void addingService(ZWaveGatewayDriver gatewayDriver)
 	{
 		gateway.set(gatewayDriver);
-
+		
 		// TODO: remove!!!
 		network.set(gateway.get().getNetwork());
-
+		
 		// this.registerDriver();
-
+		
 	}
-
+	
 	public void removedService(ZWaveGatewayDriver gatewayDriver)
 	{
 		if (gateway.compareAndSet(gatewayDriver, null))
 			// unregisters this driver from the OSGi framework
 			unRegister();
 	}
-
+	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public synchronized int match(ServiceReference reference) throws Exception
 	{
 		int matchValue = Device.MATCH_NONE;
-
-		// get the given device category
-		String deviceCategory = (String) reference
-				.getProperty(DeviceCostants.DEVICE_CATEGORY);
-
-		// get the given device manufacturer
-		String manifacturer = (String) reference
-				.getProperty(DeviceCostants.MANUFACTURER);
-
-		// get the gateway to which the device is connected
-		@SuppressWarnings("unchecked")
-		String gateway = (String) ((ControllableDevice) context
-				.getService(reference)).getDeviceDescriptor().getGateway();
-
-		// compute the matching score between the given device and this driver
-		if (deviceCategory != null)
+		
+		if ((this.network != null) && (this.gateway != null) && (this.regDriver != null))
 		{
-			if (manifacturer != null
-					&& (gateway != null)
-					&& (manifacturer.equals(ZWaveInfo.MANUFACTURER))
-					&& (thermostaticRadiatorValveDeviceCategories
-							.contains(deviceCategory))
-					&& (this.gateway.get().isGatewayAvailable(gateway)))
+			// get the given device category
+			String deviceCategory = (String) reference.getProperty(DeviceCostants.DEVICE_CATEGORY);
+			
+			try
 			{
-				matchValue = ThermostaticRadiatorValve.MATCH_MANUFACTURER
-						+ ThermostaticRadiatorValve.MATCH_TYPE;
+				// get the device class
+				if (Controllable.class.isAssignableFrom(ZWaveThermostaticRadiatorValveDriver.class.getClassLoader()
+						.loadClass(deviceCategory)))
+				{
+					// get the given device manufacturer
+					String manifacturer = (String) reference.getProperty(DeviceCostants.MANUFACTURER);
+					
+					// get the gateway to which the device is connected
+					String gateway = (String) reference.getProperty(DeviceCostants.GATEWAY);
+					
+					// compute the matching score between the given device and
+					// this driver
+					if (deviceCategory != null)
+					{
+						if (manifacturer != null && (gateway != null) && (manifacturer.equals(ZWaveInfo.MANUFACTURER))
+								&& (thermostaticRadiatorValveDeviceCategories.contains(deviceCategory))
+								&& (this.gateway.get().isGatewayAvailable(gateway)))
+						{
+							matchValue = ThermostaticRadiatorValve.MATCH_MANUFACTURER
+									+ ThermostaticRadiatorValve.MATCH_TYPE;
+						}
+						
+					}
+					
+				}
 			}
-
+			catch (ClassNotFoundException e)
+			{
+				// skip --> no match
+			}
 		}
+		
 		return matchValue;
 	}
-
+	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
-	public synchronized String attach(ServiceReference reference)
-			throws Exception
+	public synchronized String attach(ServiceReference reference) throws Exception
 	{
 		// get the referenced device
-		ControllableDevice device = ((ControllableDevice) context
-				.getService(reference));
-
+		ControllableDevice device = ((ControllableDevice) context.getService(reference));
+		
 		// get the gateway to which the device is connected
 		String gateway = (String) device.getDeviceDescriptor().getGateway();
-
+		
 		// get the corresponding end point set
-		Set<String> nodeIdSet = device.getDeviceDescriptor()
-				.getSimpleConfigurationParams().get(ZWaveInfo.NODE_ID);
-
+		Set<String> nodeIdSet = device.getDeviceDescriptor().getSimpleConfigurationParams().get(ZWaveInfo.NODE_ID);
+		
 		// get the corresponding end point set
-		Set<String> instanceIdSet = device.getDeviceDescriptor()
-				.getSimpleConfigurationParams().get(ZWaveInfo.INSTANCE_ID);
-
+		Set<String> instanceIdSet = device.getDeviceDescriptor().getSimpleConfigurationParams()
+				.get(ZWaveInfo.INSTANCE_ID);
+		
 		// get the nodeId
 		String sNodeID = nodeIdSet.iterator().next();
-
+		
 		// get the list of instances available
 		HashSet<Integer> instancesId = new HashSet<Integer>();
 		for (String sInstanceId : instanceIdSet)
 			instancesId.add(Integer.parseInt(sInstanceId));
-
+		
 		// create a new driver instance
 		ZWaveThermostaticRadiatorValveInstance driverInstance = new ZWaveThermostaticRadiatorValveInstance(
-				network.get(), device, Integer.parseInt(sNodeID), instancesId,
-				this.gateway.get().getSpecificGateway(gateway).getNodeInfo()
-						.getDeviceNodeId(), this.updateTimeMillis,
+				network.get(), device, Integer.parseInt(sNodeID), instancesId, this.gateway.get()
+						.getSpecificGateway(gateway).getNodeInfo().getDeviceNodeId(), this.updateTimeMillis,
 				this.persistenceStoreDirectory, this.context);
-
+		
 		// connect this driver instance with the device
 		device.setDriver(driverInstance);
-
+		
 		// store a reference to the connected driver
 		synchronized (connectedDrivers)
 		{
 			connectedDrivers.add(driverInstance);
 		}
-
+		
 		return null;
 	}
-
+	
 	/**
 	 * Registers this driver in the OSGi framework, making its services
 	 * available to all the other bundles living in the same or in connected
@@ -238,19 +244,17 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 	 */
 	private void registerDriver()
 	{
-		if ((gateway.get() != null) && (network.get() != null)
-				&& (this.context != null) && (this.regDriver == null))
+		if ((gateway.get() != null) && (network.get() != null) && (this.context != null) && (this.regDriver == null))
 		{
 			// create a new property object describing this driver
 			Hashtable<String, Object> propDriver = new Hashtable<String, Object>();
 			// add the id of this driver to the properties
 			propDriver.put(DeviceCostants.DRIVER_ID, DRIVER_ID);
 			// register this driver in the OSGi framework
-			regDriver = context.registerService(Driver.class.getName(), this,
-					propDriver);
+			regDriver = context.registerService(Driver.class.getName(), this, propDriver);
 		}
 	}
-
+	
 	/**
 	 * Handle the bundle de-activation
 	 */
@@ -263,7 +267,7 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 			regDriver = null;
 		}
 	}
-
+	
 	/**
 	 * Fill a set with all the device categories whose devices can match with
 	 * this driver. Automatically retrieve the device categories list by reading
@@ -271,26 +275,23 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 	 */
 	private void properFillDeviceCategories()
 	{
-		for (Class<?> devCat : ZWaveThermostaticRadiatorValveInstance.class
-				.getInterfaces())
+		for (Class<?> devCat : ZWaveThermostaticRadiatorValveInstance.class.getInterfaces())
 		{
 			thermostaticRadiatorValveDeviceCategories.add(devCat.getName());
 		}
 	}
-
+	
 	@Override
-	public void updated(Dictionary<String, ?> properties)
-			throws ConfigurationException
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException
 	{
 		if (properties != null)
 		{
 			// try to get the baseline polling time
-			String updateTimeAsString = (String) properties
-					.get(ZWaveInfo.PROPERTY_UPDATETIME);
-
+			String updateTimeAsString = (String) properties.get(ZWaveInfo.PROPERTY_UPDATETIME);
+			
 			// trim leading and trailing spaces
 			updateTimeAsString = updateTimeAsString.trim();
-
+			
 			// check not null
 			if (updateTimeAsString != null)
 			{
@@ -299,52 +300,45 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 			}
 			else
 			{
-				throw new ConfigurationException(ZWaveInfo.PROPERTY_UPDATETIME,
-						ZWaveInfo.PROPERTY_UPDATETIME
-								+ " not defined in configuraton file for "
-								+ ZWaveThermostaticRadiatorValveDriver.class
-										.getName());
+				throw new ConfigurationException(ZWaveInfo.PROPERTY_UPDATETIME, ZWaveInfo.PROPERTY_UPDATETIME
+						+ " not defined in configuraton file for "
+						+ ZWaveThermostaticRadiatorValveDriver.class.getName());
 			}
-
+			
 			// try to get the persistence store directory
-			String persistenceDir = (String) properties
-					.get(ZWaveInfo.PROPERTY_PERSISTENT_STORE);
-
+			String persistenceDir = (String) properties.get(ZWaveInfo.PROPERTY_PERSISTENT_STORE);
+			
 			// trim leading and trailing spaces
 			persistenceDir = persistenceDir.trim();
-
+			
 			// check not null
 			if (persistenceDir != null)
 			{
 				// parse the string
 				this.persistenceStoreDirectory = persistenceDir;
-
+				
 				// check absolute vs relative
-				File persistenceStoreDir = new File(
-						this.persistenceStoreDirectory);
+				File persistenceStoreDir = new File(this.persistenceStoreDirectory);
 				if (!persistenceStoreDir.isAbsolute())
-					this.persistenceStoreDirectory = System
-							.getProperty("configFolder")
-							+ "/"
+					this.persistenceStoreDirectory = System.getProperty("configFolder") + "/"
 							+ this.persistenceStoreDirectory;
 			}
-
+			
 			this.registerDriver();
 		}
 	}
-
+	
 	@Override
 	public void handleEvent(Event event)
 	{
 		// check that the received event is a clock notification
 		final Object eventContent = event.getProperty(EventConstants.EVENT);
-
+		
 		if (eventContent instanceof ClockTimeNotification)
 		{
 			// handle the clock notification asynchronously?
-			Runnable clockNotificationTask = new Runnable()
-			{
-
+			Runnable clockNotificationTask = new Runnable() {
+				
 				@Override
 				public void run()
 				{
@@ -353,23 +347,21 @@ public class ZWaveThermostaticRadiatorValveDriver implements Driver,
 						// dispatch the event content
 						for (ZWaveThermostaticRadiatorValveInstance driverInstance : connectedDrivers)
 						{
-							driverInstance
-									.checkTime(((ClockTimeNotification) eventContent)
-											.getClockTick());
+							driverInstance.checkTime(((ClockTimeNotification) eventContent).getClockTick());
 						}
 					}
-
+					
 				}
 			};
-
+			
 			// create an execution thread for event handling
 			Thread taskRunner = new Thread(clockNotificationTask);
-
+			
 			// start the task
 			taskRunner.start();
-
+			
 		}
-
+		
 	}
-
+	
 }
