0e072106f940145b9f37806756c252729efb6c4c
Luigi <luigi.derussis@polito.it>
2014-01-15 13:38:52 +0100

    experiments w/ angularjs and jetty

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,5 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  /*$routeProvider.otherwise({redirectTo: '/view1'});*/
 }]);
