e61147f87c857f6baf8e56253acd3dad1ba8b256
Luigi <luigidr@gmail.com>
2014-01-21 21:41:33 +0100

    some improvements: better parsing of JSON APIs and successfully handling status update in the UI

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,15 +10,30 @@ angular.module('dogUI.controllers', [])
 
   }])
   .controller('DeviceListCtrl', ['$scope', 'Device', 'DeviceStatus', '$timeout', function($scope, Device, DeviceStatus, $timeout) {
-    var devicesList = Device.get(function() {
-	  $scope.devices = devicesList.devices;
+	  $scope.data = {};
+	  $scope.data.devices = {};
+	  
+	  var devicesList = Device.get(function() {
+        for(var num in devicesList.devices) {
+          var currentDevice = new Device();
+          currentDevice.id = devicesList.devices[num].id;
+          currentDevice.isIn = devicesList.devices[num].isIn;
+          currentDevice.description = devicesList.devices[num].description;
+          currentDevice.controlFunctionality = devicesList.devices[num].controlFunctionality;
+          $scope.data.devices[devicesList.devices[num].id] = currentDevice;
+      }
+      
 	});
     
     var poll = function() {
         $timeout(function() {
-            $scope.deviceStatus = DeviceStatus.get();
+            var deviceStatus = DeviceStatus.get(function(){
+              for(var num in deviceStatus.devices) {
+                $scope.data.devices[deviceStatus.devices[num].id].status = deviceStatus.devices[num].status;
+              }
+            });
             poll();
-        }, 5000);
+        }, 1000);
     };     
    poll();
   }]);
