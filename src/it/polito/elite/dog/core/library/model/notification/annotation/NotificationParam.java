cd96a1db2569a7abda961d146e0d4a58136d1245
Dario Bonino <dario.bonino@polito.it>
2014-07-08 17:13:34 +0200

    Added the annotation class

---
+++ src/it/polito/elite/dog/core/library/model/notification/annotation/NotificationParam.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package it.polito.elite.dog.core.library.model.notification.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author bonino
+ *
+ */
+@Documented
+@Target(value = { ElementType.METHOD })
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface NotificationParam
+{
+	public String value();
+}
