c52d5fa1e3e4995142ea070a8b589d07314fb126
Luigi <luigi.derussis@polito.it>
2014-09-24 14:24:05 +0200

    back to unitOfMeasure instead of measuredIn

--- it.polito.elite.dog.drivers.echelon.ilon100.network/src/it/polito/elite/dog/drivers/echelon/ilon100/network/info/EchelonIlonInfo.java
+++ it.polito.elite.dog.drivers.echelon.ilon100.network/src/it/polito/elite/dog/drivers/echelon/ilon100/network/info/EchelonIlonInfo.java
@@ -44,7 +44,7 @@ public final class EchelonIlonInfo
 	public static String DATAPOINT_ID = "datapointId";
 	
 	//the datapoint unit of measure
-	public static String DATAPOINT_UOM = "measuredIn";
+	public static String DATAPOINT_UOM = "unitOfMeasure";
 	
 	//the datapoint alias
 	public static String DATAPOINT_ALIAS = "datapointAlias";
